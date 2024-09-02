bf3dbeac5b38aca149fbbd76e047df4110b5c5a8
Mahi <165472409+Dice101mahi@users.noreply.github.com>
2024-09-03 02:08:26 +0600

    1

---
+++ Alternating_Sum_of_Numbers.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define llu long long unsigned
+#define ld long double
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+#define pb push_back
+#define bp pop_back
+#define pf push_front
+#define fp pop_front
+#define pp pop_up
+#define mp make_pair
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+#define min4(a,b,c,d) min(min(a,b),min(c,d))
+#define max4(a,b,c,d) max(max(a,b),max(c,d))
+#define max5(a,b,c,d,e) max(max3(a,b,c),max(d,e))
+#define min5(a,b,c,d,e) min(min3(a,b,c),min(d,e))
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+const int MOD = 1000000007;
+#define  in(a, m)  for(int i = 0 ; i < m ; i++) cin>>a[i]
+#define fm(i, m, n) for(int (i) = 0 ; (i) < (m) ; i += n)
+#define fn(i, n, m) for(int (i) = (n) ; (i) < (m) ; (i)++)
+#define f(i, m) for(int (i) = 0 ; (i) < (m) ; (i)++)
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+
+void init_code(){  
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main(){
+    fast
+    init_code();
+
+    ll t, m, n, o, p;
+
+    cin>>t;
+
+    while(t--){
+        cin>>m;
+
+        int a[m];
+        in(a, m);
+
+        n = 0;
+        f(i, m){
+            if(i%2 == 0){
+                n += a[i];
+            }else{
+                n -= a[i];
+            }
+        }
+
+        cout<<n<<endl;
+    }
+
+    return 0;
+}
