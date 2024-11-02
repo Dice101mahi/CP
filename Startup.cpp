afbf5f51e199ab6e35a1f6da131c55f4b4bd95de
Mahi <165472409+Dice101mahi@users.noreply.github.com>
2024-11-03 02:28:56 +0600

    Create Startup.cpp

---
+++ Startup.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define llu long long unsigned
+#define ld double
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+#define pb push_back
+#define bp pop_back
+#define pf push_front
+#define fp pop_front
+#define pp pop_up
+#define mp make_pair
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+#define min4(a,b,c,d) min(min(a,b),min(c,d))
+#define max4(a,b,c,d) max(max(a,b),max(c,d))
+#define max5(a,b,c,d,e) max(max3(a,b,c),max(d,e))
+#define min5(a,b,c,d,e) min(min3(a,b,c),min(d,e))
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+const int MOD = 1000000007;
+#define in(a, m)  for(int i = 0 ; i < m ; i++) cin>>a[i]
+#define fm(i, m, n) for(int (i) = 0 ; (i) < (m) ; i += n)
+#define fn(i, n, m) for(int (i) = (n) ; (i) < (m) ; (i)++)
+#define f(i, m) for(int (i) = 0 ; (i) < (m) ; (i)++)
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+
+int main(){
+    fast
+
+    ll t, m, n, o, p;
+    int f;
+
+    cin>>t;
+
+    while(t--){
+        cin>>m>>n;
+
+        map<int, int> mp;
+
+        f(i, n){
+            cin>>o>>p;
+            
+            if(mp.find(o) == mp.end()){
+                mp[o] = p;
+            }else{
+                mp[o] += p;
+            } 
+        }
+
+        vi v;
+        for(auto i : mp){
+            v.push_back(i.second);
+        }
+
+        sort(v.begin(), v.end(), greater<int>());
+
+        ll ans = 0;
+
+        f(i, min<ll>(v.size(), m)){
+            ans += v[i];
+        }
+
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
